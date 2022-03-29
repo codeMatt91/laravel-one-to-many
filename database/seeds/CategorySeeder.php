a4d838d9fcf35c53ab49f25e4def1fa08abbd00d
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-29 16:08:42 +0200

    created seeder to populate table

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        // Creo un array associativo
+        $categories = [
+            ['label' => 'Film', 'color' => 'info'],
+            ['label' => 'Anime', 'color' => 'warning'],
+            ['label' => 'Actuality', 'color' => 'secondary'],
+            ['label' => 'Science', 'color' => 'success'],
+            ['label' => 'Food', 'color' => 'red'],
+            ['label' => 'Serie', 'color' => 'black'],
+            ['label' => 'Economy', 'color' => 'orange'],
+        ];
+
+        // Ciclo sull'array per riempire i campi della tabella creando un istanza della categoria 
+        foreach($categories as $category){
+            $c = new Category();
+            $c->label = $category['label'];
+            $c->color = $category['color'];
+            $c->save();
+        }
+
     }
 }
