b9151361f011c8fe2a74e3c0267ccddc2f57d4d5
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-29 15:39:25 +0200

    created migration category table

--- database/migrations/2022_03_29_133354_create_categories_table.php
+++ database/migrations/2022_03_29_133354_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('label', 30);
+            $table->string('color', 20);
             $table->timestamps();
         });
     }
